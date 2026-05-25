11319e7ffdf7a6b0a330649d755c252762c068b8
mentoriaavirada-star <mentoriaavirada@gmail.com>
2026-05-24 23:55:24 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/pdi/(.*)",
-]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/pdi/(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    await (await auth()).protect();
+    const authObj = await auth();
+    authObj.protect();
   }
 });
 
